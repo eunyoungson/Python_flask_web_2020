b7c6fc9ca84f7c8f73dda00489f57aa059bdeb0c
Eunyoung Son <lp0170@gmail.com>
2021-02-01 18:31:24 +0900

    0201

--- 03_Module/bp7_advanced/aclsf.py
+++ 03_Module/bp7_advanced/aclsf.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 from my_util.weather import get_weather
 import re
 from sklearn.pipeline import Pipeline
+from konlpy.tag import Okt
 
 aclsf_bp = Blueprint('aclsf_bp', __name__)
 
@@ -111,5 +112,58 @@ def news():
                        'pred_cl':pred_cl,
                        'pred_tl': pred_tl}
         
-        return render_template('advanced/imdb_res.html', menu=menu, review=test_data[0], index=index,
+        return render_template('advanced/imdb_res.html', menu=menu, review=test_data[0], 
+                                res=result_dict, weather=get_weather())
+
+@aclsf_bp.route('/naver', methods=['GET', 'POST'])
+def naver():
+    menu = {'ho':0, 'da':0, 'ml':10, 
+            'se':0, 'co':0, 'cg':0, 'cr':0, 'wc':0,'st':0,
+            'cf':0, 'ac':1, 're':0, 'cu':0}
+    
+    if request.method == 'GET':
+        return render_template('advanced/naverR.html',menu=menu, weather=get_weather())
+    else: 
+        
+       
+        if request.form['optradio'] == 'index':
+            #int로 들어올때
+            index = int(request.form['index'] or 0)
+            df = pd.read_csv('F:/workspace/machine-Learning/00.data/naverMovie/test.tsv' ,sep='\t')
+            #test_data.append(df.iloc[index,2])  
+            org_review = df.document[index]
+            state = '긍정' if df.label[index] else '부정'
+        else:
+            #직접입력
+            org_review = request.form['review']
+            state = '직접 입력'
+            
+
+        
+        test_data = []
+        
+        review= re.sub("[^ㄱ-ㅎㅏ-ㅣ가-힣 ]","",org_review)
+        okt = Okt()
+        morphs =okt.morphs(review,stem= True)
+        stopwords = [ '의','가','이','은','들','는','좀','잘','걍','과','도','를','으로','자','에','와','한','하다']
+        temp_X = ' '.join([word for word in morphs if not word in stopwords])
+        test_data.append(temp_X)
+
+
+        naver_count_lr = joblib.load('static/model/Npipeline_cl.pkl')
+        naver_tfidf_lr = joblib.load('static/model/Npipeline_tl.pkl')
+        naver_count_nv = joblib.load('static/model/Npipeline_cn.pkl')
+        naver_tfidf_nv = joblib.load('static/model/Npipeline_tn.pkl')
+
+        
+        pred_cl = '긍정' if naver_count_lr.predict(test_data)[0] else '부정'
+        pred_tl = '긍정' if naver_tfidf_lr.predict(test_data)[0] else '부정'
+        pred_cn = '긍정' if naver_count_nv.predict(test_data)[0] else '부정'
+        pred_tn = '긍정' if naver_tfidf_nv.predict(test_data)[0] else '부정'
+        
+        result_dict = {'state':state, 
+                       'pred_cl':pred_cl, 'pred_cn':pred_cn, 'pred_tn': pred_tn,
+                       'pred_tl': pred_tl}
+        
+        return render_template('advanced/naverR_res.html', menu=menu, review=org_review, 
                                 res=result_dict, weather=get_weather())
